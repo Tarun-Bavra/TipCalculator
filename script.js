3fc59aaaa56eb76a9d808291060dc2e109f83b33
Tarun <tarunbavra@gmail.com>
2024-06-29 12:00:23 +0530

    tip-calculator

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-let inp = document.querySelector("input")
-console.log(inp);
+let inp = document.querySelector("input");
+let output = document.querySelector(".tip");
+let btn = document.querySelector("button");
+btn.addEventListener("click", calculateTip);
+function calculateTip() {
+  let bill = inp.value;
+  let tip = bill * 0.02;
+  output.innerHTML = `<p>Your Tip should be Rupees ${tip}</p>`;
+}
